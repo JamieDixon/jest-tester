b68cc145f539661994fcceca881e7b6c39f53454
Santiago de Leon <sdeleon28@gmail.com>
2017-08-16 11:40:40 -0300

    Customized babel build so it resembles hivehome

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import core from '../../core';
+import core from 'core';
 
 class App extends Component {
   render() {
